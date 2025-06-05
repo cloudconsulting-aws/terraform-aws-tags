46e2b580aa7eb8b4aeeddb2da260456fd1a9d970
Asaph Tinoco <asaph@sendwave.com>
2025-06-05 08:17:02 +0200

    IAWS2: adding tags

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 locals {
   default = {
-    "Terraform"   = var.deployed_with_terraform
-    "Environment" = var.environment
-    "Owner"       = var.owner
-    "Repository"  = var.repository
-    "Cost Center" = var.cost_center
-    "Project"     = var.project_name
+    "Terraform"      = var.deployed_with_terraform
+    "Environment"    = var.environment
+    "Owner"          = var.owner
+    "Repository"     = var.repository
+    "Cost Center"    = var.cost_center
+    "Project"        = var.project_name
+    "Confidential"   = var.confidential ? "Yes" : "No"
+    "Backup Enabled" = var.backup_enabled ? "Yes" : "No"
   }
 }
